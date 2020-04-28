c26d3a99b44ec0c79006297c9dcf8ad3d8cfabaf
ghazale <ghazalze@yahoo.com>
2020-04-28 14:48:38 +0430

    DBLab3 is added
    2 file

---
+++ Lab3/dblab3CreatedLogin.sql
@@ -0,0 +1,8 @@
+create table ATableForTest(
+id char(6),
+name varchar(20)
+);
+
+select * from ATableForTest;
+
+insert into ATableForTest values('123456','someOne');
---
+++ Lab3/dblab3mainconnection.sql
@@ -0,0 +1,28 @@
+
+-- Q1**************
+--create login ghazale
+--with password ='123'
+
+--create server role Role1
+
+--grant create any database to Role1
+
+--alter server role dbcreator drop member ghazale
+
+--alter server role Role1 add member ghazale
+
+--use AdventureWorks2012
+--go
+
+--create user gh1 for login ghazale
+
+--Q2************************************
+--create role Role2
+--create user gh2 for login ghazale
+--alter role Role2 add member gh2
+
+alter role db_securityadmin add member Role2
+
+alter role db_datareader add member Role2
+
+
