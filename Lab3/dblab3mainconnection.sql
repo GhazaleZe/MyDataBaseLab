2c0ff3b222bed953c542670af45c87cda3038e6e
Ghazale Zehtab <41547574+GhazaleZe@users.noreply.github.com>
2020-04-28 14:50:48 +0430

    Update dblab3mainconnection.sql

--- Lab3/dblab3mainconnection.sql
+++ Lab3/dblab3mainconnection.sql
@@ -1,25 +1,25 @@
 
 -- Q1**************
---create login ghazale
---with password ='123'
+create login ghazale
+with password ='123'
 
---create server role Role1
+create server role Role1
 
---grant create any database to Role1
+grant create any database to Role1
 
---alter server role dbcreator drop member ghazale
+alter server role dbcreator drop member ghazale
 
---alter server role Role1 add member ghazale
+alter server role Role1 add member ghazale
 
---use AdventureWorks2012
---go
+use AdventureWorks2012
+go
 
---create user gh1 for login ghazale
+create user gh1 for login ghazale
 
 --Q2************************************
---create role Role2
---create user gh2 for login ghazale
---alter role Role2 add member gh2
+create role Role2
+create user gh2 for login ghazale
+alter role Role2 add member gh2
 
 alter role db_securityadmin add member Role2
 
